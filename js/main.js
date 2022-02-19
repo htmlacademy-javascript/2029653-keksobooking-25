4b1a182f857e27004ed9b6e2e9d3704ce4dafbae
Andrei.Shubin <andrei.shubin@travelline.ru>
2022-02-20 01:56:48 +0300

    Созданы функции для получения случайных чисел.

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function getRandomInt(min, max) {
+  return Math.floor((min < max) ? Math.random() * (max - min) + min : Math.random() * (min - max) + max);
+}
+
+function getRandomFloat(min, max, symbolsAfterDot) {
+  const multiplier = Math.pow(10, symbolsAfterDot);
+  let numberFrom = min;
+  let numberTo = max;
+  if (max < min) {
+    numberFrom = max;
+    numberTo = min;
+  }
+  return Math.floor((Math.random() * (numberTo - numberFrom) + numberFrom) * multiplier) / multiplier;
+}
+
