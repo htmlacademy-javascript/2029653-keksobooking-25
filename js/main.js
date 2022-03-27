d3962b8aa1a5e37c9df73a3671cadf1d55dbb5c4
Andrei.Shubin <andrei.shubin@travelline.ru>
2022-03-27 21:36:56 +0300

    исправление замечания eslint

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ import {activateForm, deactivateForm, initForm} from './form.js';
 import {initMap, setCommonMarkers} from './map.js';
 import './slider.js';
 
-const hotelsLocations = createdObjects.map(value => value.location);
+const hotelsLocations = createdObjects.map((value) => value.location);
 
 deactivateForm();
 initForm();
