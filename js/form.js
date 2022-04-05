03bc9ed3e72b8e387a1c1d1cf676af6d356a5f35
Andrei.Shubin <andrei.shubin@travelline.ru>
2022-04-05 16:08:30 +0300

    Правда или действие (часть 2)

--- js/form.js
+++ js/form.js
@@ -15,9 +15,21 @@ const CAPACITY_RULES = {
   3: [3]
 };
 
+const MIN_PRICE_RULES = {
+  'bungalow': 0,
+  'flat': 1000,
+  'hotel': 3000,
+  'house': 5000,
+  'palace': 10000,
+};
+
 const adFormElement = document.querySelector('.ad-form');
 const roomNumberElement = document.querySelector('#room_number');
 const capacityElement = document.querySelector('#capacity');
+const housingTypeElement = document.querySelector('#type');
+const priceElement = document.querySelector('#price');
+const timeinElement = document.querySelector('#timein');
+const timeoutElement = document.querySelector('#timeout');
 
 const adFormValidator = new Pristine(adFormElement, {
   classTo: 'ad-form__element',
@@ -33,7 +45,6 @@ const deactivateForm = () => {
   activeElements.forEach((element) => {
     element.classList.add('disabled');
   });
-
 };
 
 const activateForm = () => {
@@ -53,9 +64,15 @@ const validateCapacity = (capacity) => {
   return CAPACITY_RULES[capacity].includes(roomNumberValue);
 };
 
+const validateMinPrice = (price) => {
+  const selectedHousingType = housingTypeElement.options[housingTypeElement.selectedIndex].value;
+  return price >= MIN_PRICE_RULES[selectedHousingType];
+};
+
 const initForm = () => {
   adFormValidator.addValidator(roomNumberElement, validateRoomNumber, 'выберите другое количество комнат');
   adFormValidator.addValidator(capacityElement, validateCapacity, 'или выберите другое количество мест');
+  adFormValidator.addValidator(priceElement, validateMinPrice, 'цена должна быть выше');
 
   adFormElement.addEventListener('submit', (evt) => {
     evt.preventDefault();
@@ -64,5 +81,20 @@ const initForm = () => {
     }
   });
 };
+
+const changePricePlaceholder = () => {
+  const selectedHousingType = housingTypeElement.options[housingTypeElement.selectedIndex].value;
+  priceElement.placeholder = MIN_PRICE_RULES[selectedHousingType];
+};
+
+housingTypeElement.addEventListener('change', changePricePlaceholder);
+
+timeinElement.addEventListener('change', () => {
+  timeoutElement.selectedIndex = timeinElement.selectedIndex;
+});
+
+timeoutElement.addEventListener('change', () => {
+  timeinElement.selectedIndex = timeoutElement.selectedIndex;
+});
 
 export {activateForm, deactivateForm, initForm};
