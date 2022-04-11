94d87ed95168a0cf2b65b3521371e9ee4ce2dbe0
Andrei.Shubin <andrei.shubin@travelline.ru>
2022-04-11 20:34:59 +0300

    Удалена неиспользуемая константа

--- js/constants.js
+++ js/constants.js
@@ -1,7 +1,7 @@
-const HOTEL_TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 const CHECKIN_TIMES = ['12:00', '13:00', '14:00'];
 const CHECKOUT_TIMES = ['12:00', '13:00', '14:00'];
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const HOTEL_TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
@@ -18,9 +18,6 @@ const MAX_ROOMS_COUNT = 6;
 
 const MIN_GUESTS_COUNT = 1;
 const MAX_GUESTS_COUNT = 6;
-
-const OBJECT_COUNT = 10;
-
 
 const TOKYO_COORDINATES = {
   lat: 35.652832,
@@ -42,10 +39,10 @@ const DATA_URL = 'https://25.javascript.pages.academy/keksobooking/';
 const ALERT_SHOW_TIME = 3000;
 
 export {
-  HOTEL_TYPES,
   CHECKIN_TIMES,
   CHECKOUT_TIMES,
   FEATURES,
+  HOTEL_TYPES,
   PHOTOS,
   PHOTOS_LENGTH,
   MIN_PRICE,
@@ -54,7 +51,6 @@ export {
   MAX_ROOMS_COUNT,
   MIN_GUESTS_COUNT,
   MAX_GUESTS_COUNT,
-  OBJECT_COUNT,
   DEFAULT_COORDINATES,
   APARTMENT_TYPES,
   HOTELS_DATA_URL,
